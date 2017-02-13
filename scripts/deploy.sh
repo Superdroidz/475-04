8c05ed3bed7ae74382fab8cc62ee4b053bb8f4fd
Adam Gestwa <adam.l.gestwa@gmail.com>
2017-02-13 18:34:09 +0000

    Update deploy.sh
    Edited push command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,14 +5,14 @@ if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
   git init
 
   git config user.name "Travis CI"
-  git config user.email "feriese@microsoft.com"
+  git config user.email "nothing152351252135@nothing.com"
 
   git add .
   git commit -m "Deploy"
 
   # We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+  git push -f -q https://Superdroidz:$GITHUB_API_KEY@github.com/Superdroidz/475-04 master:master > /dev/null 2>&1 
 else
   echo 'Invalid branch. You can only deploy from gh-pages.'
   exit 1
